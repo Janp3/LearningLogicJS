fd9b5c2d6bfdf07c27a0f1ee796e1b060e8fa8ba
Jean <jean.c.drawing@gmail.com>
2023-01-26 11:07:50 -0300

    ex02-done

--- ex02/ex02.js
+++ ex02/ex02.js
@@ -1,14 +1,3 @@
-function getHigher(){
-    var a = document.getElementById('n1').value
-    var b = document.getElementById('n2').value
-    var result = document.getElementById('result01')
-    if(Number(a) < Number(b)){
-        return result.innerHTML = 'Result: ' + b
-    }else {
-        return result.innerHTML = 'Result: ' + a
-    }
-}
-
 function getGender(){
     var input = document.getElementById('txt2').value.toUpperCase()
     var result02 = document.getElementById('result02')
